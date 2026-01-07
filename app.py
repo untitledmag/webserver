ca37c6b8f50198e1e0f08523137727eb3335f53e
Manreet Singh <singh.manreet5@gmail.com>
2026-01-07 08:34:18 -0500

    Added authentication

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
 import requests
 from config import port, host, debug
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, Response
+from functools import wraps
+import base64
 
 def get_random_cat_image():
     # No API key needed for basic random search
     resp = requests.get('https://api.thecatapi.com/v1/images/search')
     data = resp.json()
     return data[0]['url']
+
+USERNAME = 'meow'
+PASSWORD = 'meow67'
+
+def check_auth(auth_header:str)->bool:
+    try:
+        schema, credentials = auth_header.split()
+        if schema.lower() != 'basic':
+            return False
+        decoded = base64.b64decode(credentials).decode('utf-8')
+        username, password = decoded.split(":", 1)
+        return username==USERNAME and password==PASSWORD
+    except Exception:
+        pass
+
+def authenticate():
+    return Response(
+        'Authentication required',
+        401,
+        {"WWW-Authenticate": 'Basic realm="API"'}
+    )
+
+
+def auth_protected(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not check_auth(auth_header):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 
 app = Flask(__name__)
 
@@ -39,6 +74,7 @@ def get_developer():
     return jsonify(data)
 
 @app.route('/api/v1/get-cat')
+@auth_protected
 def get_cat_url():
     url = get_random_cat_image()
     data = {
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import requests
+import base64
+
 url = "http://127.0.0.1:8080/api/v1/get-cat"
 
+credentials = "meow:meow67"
+# Add .decode('utf-8') at the end here
+encoded = base64.b64encode(credentials.encode()).decode('utf-8')
+
+headers = {
+    'Authorization': f'Basic {encoded}'
+}
+
 try:
-    response = requests.get(url)
-    # Check if the request was successful
+    response = requests.get(url, headers=headers)
     response.raise_for_status() 
     
     data = response.json()
-    print(data)
+    print("Success! Cat URL:", data['url'])
 except requests.exceptions.RequestException as e:
     print(f"Error connecting to local server: {e}")
